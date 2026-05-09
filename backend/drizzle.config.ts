d48f7be693a52e57e869df093517257587ff2000
Anry_404 <darkanry88@gmail.com>
2026-05-09 09:07:00 +0200

    chore: use tsx for migrations and restore original drizzle config

--- backend/drizzle.config.ts
+++ backend/drizzle.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'drizzle-kit';
+
 export default defineConfig({
-  schema: './src/db/schema/*.ts',
+  schema: './src/db/schema/**/!(index).ts',
   out: './src/db/migrations',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_URL || '',
+    url: process.env.DATABASE_URL!,
   },
 });
